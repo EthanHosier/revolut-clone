1800e2d092979ee345791fbd0a83e1c60ddaeeac
Ethan Hosier <ethanhosier@ethans-mac-mini.home>
2024-03-30 03:14:01 +0000

    add initial login / signup screens, no logic yet

---
+++ app/login.tsx
@@ -0,0 +1,166 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import React, { useState } from "react";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+enum SignInType {
+  Phone,
+  Email,
+  Google,
+  Apple,
+}
+
+const Page = () => {
+  const [countryCode, setCountryCode] = useState("+44");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const onSignIn = async (type: SignInType) => {
+    if (type === SignInType.Phone) {
+      console.log("Phone Sign In");
+    }
+  };
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 90 : 0}
+    >
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Welcome Back!</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter the phone nunmber associated with your account.
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Country code"
+            placeholderTextColor={Colors.gray}
+            value={countryCode}
+          />
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="Mobile number"
+            placeholderTextColor={Colors.gray}
+            keyboardType="numeric"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+            phoneNumber !== "" ? styles.enabled : styles.disabled,
+            { marginBottom: 20 },
+          ]}
+          onPress={() => onSignIn(SignInType.Phone)}
+        >
+          <Text style={defaultStyles.buttonText}>Continue</Text>
+        </TouchableOpacity>
+
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 16 }}>
+          <View
+            style={{
+              flex: 1,
+              height: StyleSheet.hairlineWidth,
+              backgroundColor: Colors.gray,
+            }}
+          />
+          <Text style={{ color: Colors.gray, fontSize: 20 }}>or</Text>
+          <View
+            style={{
+              flex: 1,
+              height: StyleSheet.hairlineWidth,
+              backgroundColor: Colors.gray,
+            }}
+          />
+        </View>
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+            {
+              flexDirection: "row",
+              gap: 16,
+              marginTop: 20,
+              backgroundColor: "#fff",
+            },
+          ]}
+          onPress={() => onSignIn(SignInType.Email)}
+        >
+          <Ionicons name="mail" size={24} color={"#000"} />
+          <Text style={[defaultStyles.buttonText, { color: "#000" }]}>
+            Continue with Email
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+            {
+              flexDirection: "row",
+              gap: 16,
+              marginTop: 20,
+              backgroundColor: "#fff",
+            },
+          ]}
+          onPress={() => onSignIn(SignInType.Google)}
+        >
+          <Ionicons name="logo-google" size={24} color={"#000"} />
+          <Text style={[defaultStyles.buttonText, { color: "#000" }]}>
+            Continue with Google
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+            {
+              flexDirection: "row",
+              gap: 16,
+              marginTop: 20,
+              backgroundColor: "#fff",
+            },
+          ]}
+          onPress={() => onSignIn(SignInType.Apple)}
+        >
+          <Ionicons name="logo-apple" size={24} color={"#000"} />
+          <Text style={[defaultStyles.buttonText, { color: "#000" }]}>
+            Continue with Apple
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
+
+export default Page;
