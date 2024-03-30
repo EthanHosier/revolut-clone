b696d95282c927eb5a8c9755beef60056b4d80f9
Ethan Hosier <ethanhosier@ethans-mac-mini.home>
2024-03-30 23:17:02 +0000

    add drag and drop and transactions

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,12 +1,114 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import Colors from "@/constants/Colors";
+import RoundBtn from "@/components/RoundBtn";
+import Dropdown from "@/components/Dropdown";
+import { useBalanceStore } from "@/store/balance-store";
+import { defaultStyles } from "@/constants/Styles";
+import { Ionicons } from "@expo/vector-icons";
+import WidgetList from "@/components/SortableList/WidgetList";
 
 const Page = () => {
+  const { balance, runTransaction, transactions, clearTransactions } =
+    useBalanceStore();
+
+  const onAddMoney = () => {
+    runTransaction({
+      id: Math.random().toString(),
+      amount: 100,
+      date: new Date(),
+      title: "Added money",
+    });
+  };
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: Colors.background }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance()}</Text>
+          <Text style={styles.currency}>£</Text>
+        </View>
+      </View>
+
+      <View style={styles.actionRow}>
+        <RoundBtn icon="add" text="Add money" onPress={onAddMoney} />
+        <RoundBtn icon="refresh" text="Exchange" onPress={clearTransactions} />
+        <RoundBtn icon="list" text="Details" />
+        <Dropdown />
+      </View>
+
+      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+      <View style={styles.transactions}>
+        {transactions.length === 0 && (
+          <Text style={{ padding: 14, color: Colors.gray }}>
+            No transactions yet
+          </Text>
+        )}
+        {transactions.map((t) => (
+          <View
+            key={t.id}
+            style={{ flexDirection: "row", alignItems: "center", gap: 16 }}
+          >
+            <View style={styles.circle}>
+              <Ionicons
+                name={t.amount > 0 ? "add" : "remove"}
+                size={24}
+                color={Colors.dark}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontWeight: "500" }}>{t.title}</Text>
+              <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                {new Date(t.date).toLocaleDateString()}
+              </Text>
+            </View>
+            <Text>{t.amount}£</Text>
+          </View>
+        ))}
+      </View>
+      <Text style={defaultStyles.sectionHeader}>Widgets</Text>
+      <WidgetList />
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    gap: 10,
+  },
+  balance: { fontSize: 50, fontWeight: "bold" },
+  currency: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  transactions: {
+    marginHorizontal: 20,
+    padding: 14,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    gap: 20,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+    backgroundColor: Colors.lightGray,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Page;
